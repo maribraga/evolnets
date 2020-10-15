b6aa85939a3f1a770d1b88a050591ce4246cf03f
Mariana Braga <mpiresbr@gmail.com>
2020-10-15 11:29:12 +0200

    Add documentation for the package

---
+++ R/evolnets.R
@@ -0,0 +1,22 @@
+#' evolnets: a package for parsing outputs from inference of host-repertoire evolution
+#'
+#' The evolnets package provides three categories of important functions:
+#' rates, ancestral states and samples.
+#'
+#' @section Rates functions:
+#' The rates functions are used to calculate effective rates of host-repertoire
+#'     evolution.
+#'
+#' @section Ancestral states:
+#' The ancestral states functions are used to calculate the posterior probabilities
+#'     of host-parasite interactions at internal nodes of the parasite tree or at
+#'     specific time points in the past.
+#'
+#' @section Samples:
+#' The samples functions perform calculation for each sampled host-parasite
+#'     network during MCMC.
+#'
+#' @docType package
+#' @name evolnets
+NULL
+#> NULL
