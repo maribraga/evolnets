580eba4f058048a1dfe689058dba2e95059764ad
Wouter van der Bijl <woutervdbijl@hotmail.com>
2021-07-27 09:14:58 -0700

    Code formatting & code consistency

--- R/posterior_at_nodes.R
+++ R/posterior_at_nodes.R
@@ -26,50 +26,47 @@
 #' # at chosen internal nodes of the parasite tree
 #' nodes <- c(129:131)
 #' pp_at_nodes <- posterior_at_nodes(history, nodes, host_tree)
-
-posterior_at_nodes = function(history, nodes, host_tree, state = c(2)) {
-
+posterior_at_nodes <- function(history, nodes, host_tree, state = c(2)) {
   dat <- dplyr::filter(history, node_index %in% nodes)
-  iterations = sort(unique(dat$iteration))
-  n_iter = length(iterations)
+  iterations <- sort(unique(dat$iteration))
+  n_iter <- length(iterations)
 
   # get dimensions
-  n_host_tip = length( stringr::str_split( dat$start_state[1], "" )[[1]] )
-  n_parasite_lineage = length(unique(dat$node_index))
+  n_host_tip <- length(stringr::str_split( dat$start_state[1], "" )[[1]])
+  n_parasite_lineage <- length(unique(dat$node_index))
 
-  g = matrix(data = 0, nrow = n_parasite_lineage, ncol = n_host_tip)
+  g <- matrix(data = 0, nrow = n_parasite_lineage, ncol = n_host_tip)
 
-  array_names = list( 1:n_iter, paste0("Index_",nodes), host_tree$tip.label )
-  array = array(0, dim=c(n_iter, n_parasite_lineage, n_host_tip), dimnames=array_names)
+  array_names <- list(1:n_iter, paste0("Index_",nodes), host_tree$tip.label)
+  array <- array(0, dim = c(n_iter, n_parasite_lineage, n_host_tip), dimnames = array_names)
 
   for (i in 1:n_iter) {
-    it = iterations[i]
-    dat_it = dat[ dat$iteration == it, ]
-    ret = list()
+    it <- iterations[i]
+    dat_it <- dat[dat$iteration == it, ]
+    ret <- list()
     for (i in 1:length(nodes)) {
-      dat2 = dat_it[ dat_it$node_index == nodes[i], ]
-      if(nrow(dat2)==1){
-        ret[[i]] = dat2
-      } else{
-        ret[[i]] = dat2[ which.min(dat2$transition_time), ]
+      dat2 <- dat_it[dat_it$node_index == nodes[i], ]
+      if (nrow(dat2) == 1) {
+        ret[[i]] <- dat2
+      } else {
+        ret[[i]] <- dat2[which.min(dat2$transition_time), ]
       }
     }
 
     ret <- data.table::rbindlist(ret)
 
     for (r in 1:nrow(ret)) {
-      s = as.numeric( stringr::str_split (ret$end_state[r], "")[[1]] )
+      s <- as.numeric(stringr::str_split(ret$end_state[r], "")[[1]])
       s_idx = s %in% state
 
-      g[ r, s_idx ] = g[ r, s_idx ] + 1
-      array[i, r, s_idx] = state
-
+      g[r, s_idx ] <- g[r, s_idx ] + 1
+      array[i, r, s_idx] <- state
     }
   }
 
   # convert to probability
-  g = g * (1/n_iter)
-  row.names(g) <- paste0("Index_",nodes)
+  g = g * (1 / n_iter)
+  row.names(g) <- paste0("Index_", nodes)
   colnames(g) <- host_tree$tip.label
 
   list <- list(array, g)
