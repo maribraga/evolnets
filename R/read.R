7a44d03367819228441d1c4353061e2394817880
Mariana Braga <mpiresbr@gmail.com>
2022-02-02 13:48:12 +0200

    Add function to read tree from RevBayes

--- R/read.R
+++ R/read.R
@@ -26,3 +26,36 @@ read_history <- function(path_to_hist, burnin = 0.1){
 }
 
 node_index <- parent_index <- child1_index <- child2_index <- iteration <- NULL
+
+
+#' Read symbiont tree file produced by RevBayes containing node indices
+#'
+#' @param path_to_tree String path to the .txt file exported from RevBayes
+#'
+#' @return A `phylo` object
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' \dontrun{tree <- read_tree_from_revbayes("symbiont_tree.tre")}
+
+read_tree_from_revbayes <- function(path_to_tree){
+
+  treeRev <- treeio::read.beast.newick(path_to_tree)
+
+  tree <- treeRev@phylo
+
+  index_node <- treeRev@data %>%
+    dplyr::mutate(node = as.numeric(node)) %>%
+    dplyr::arrange(node)
+
+  indices <- index_node %>%
+    dplyr::filter(node > treeio::Ntip(tree)) %>%
+    dplyr::pull(index)
+
+  names(indices) <- NULL
+
+  tree$node.label <- paste0("Index_",indices)
+
+  return(tree)
+}
