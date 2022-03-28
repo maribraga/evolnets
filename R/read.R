b8c90f0a587fc3675b8fdbcca5c4cd9f71290566
Wouter van der Bijl <woutervdbijl@hotmail.com>
2022-03-28 16:44:20 -0700

    general cleanup, use of .data$ pronoun, input checks.

--- R/read.R
+++ R/read.R
@@ -6,8 +6,9 @@
 #' @param path_to_hist String path to the .txt file exported from RevBayes
 #' @param burnin Fraction of iterations to be removed as burnin. Default to 10%.
 #'
-#' @return A data frame
+#' @return A data.frame
 #' @importFrom magrittr %>%
+#' @importFrom rlang .data
 #' @export
 #'
 #' @examples
@@ -15,17 +16,22 @@
 #' \dontrun{history <- read_history("/path/history.txt", burnin = 0.2)}
 
 read_history <- function(path_to_hist, burnin = 0.1){
+
+  if (!is.numeric(burnin) || burnin > 1 || burnin < 0 || length(burnin) != 1) {
+    stop('`burnin` should be a numeric vector of length 1, between 0 and 1.')
+  }
+
   colclasses <- c(rep("numeric",7),"character","character","numeric","character",rep("numeric",3))
 
-  history = utils::read.table(path_to_hist, sep="\t", header=T, colClasses = colclasses) %>%
-    dplyr::mutate(node_index = node_index + 1,
-                  parent_index = parent_index + 1,
-                  child1_index = child1_index + 1,
-                  child2_index = child2_index + 1) %>%
-    dplyr::filter(iteration > max(iteration)*burnin)
+  history <- utils::read.table(path_to_hist, sep = "\t", header = TRUE, colClasses = colclasses) %>%
+    dplyr::mutate(
+      node_index = .data$node_index + 1,
+      parent_index = .data$parent_index + 1,
+      child1_index = .data$child1_index + 1,
+      child2_index = .data$child2_index + 1
+    ) %>%
+    dplyr::filter(.data$iteration > max(.data$iteration) * burnin)
 }
-
-node_index <- parent_index <- child1_index <- child2_index <- iteration <- NULL
 
 
 #' Read symbiont tree file produced by RevBayes containing node indices
@@ -55,7 +61,7 @@ read_tree_from_revbayes <- function(path_to_tree){
 
   names(indices) <- NULL
 
-  tree$node.label <- paste0("Index_",indices)
+  tree$node.label <- paste0("Index_", indices)
 
   return(tree)
 }
